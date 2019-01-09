0886b69eadb8722bfe2cebccd135ead510804ba6
paulchin <paulchin20@yahoo.com>
2019-01-09 18:01:05 +0800

    php

---
+++ 6-php/variables.php
@@ -0,0 +1,13 @@
+<?php
+  $name = "Hanchiang";
+
+  echo "<p>Hello $name</p>";
+
+  $message = "Welcome to " . $name;
+
+  echo $message;
+
+  $mynumber = 3;
+  $result = $mynumber * 4;
+  echo "<br>The result of $mynumber x 4 is: " . $result
+?>
